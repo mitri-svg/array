e49396abd41f031bd0563d67289a39042e0d2481
mitri-svg <hmirseid54@gmail.com>
2024-11-25 09:54:37 -0800

    .

---
+++ script.js
@@ -0,0 +1,18 @@
+// let oyunlar = ['futbol', 'basketboll', "voleyball"]
+
+// let bazar = {
+//     xiyar : "patron",
+//     pomidor: "qirmizi",
+//     biber: "yasil"
+// }
+
+// console.log(the_cafi.aysel.otaqda_olanlar[5][0].soyad);
+
+
+const answer = []
+
+answer[0] = prompt("Adivizi daxil edin: ")
+answer[1] = prompt("Soyadinizi daxil edin: ")
+answer[2] = prompt("Yasinizi daxil edin: ")
+
+document.getElementById("list").innerHTML = answerKf
